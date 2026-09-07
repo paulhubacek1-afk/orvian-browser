f2b4c5451539034f01bb654fbc87acf19772f077
paulhubacek1-afk <paul.hubacek1@gmail.com>
2026-09-07 14:47:44 +0200

    fix: disambiguate WPF panda path type

--- src/Orvian.Browser/PandaControl.cs
+++ src/Orvian.Browser/PandaControl.cs
@@ -4,6 +4,7 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using Path = System.Windows.Shapes.Path;
 
 namespace Orvian.Browser;
 
