c303a7b12580763e2714c400832b5f19e9e19505
paulhubacek1-afk <paul.hubacek1@gmail.com>
2026-09-07 16:43:53 +0200

    fix: wire settings close and password vault actions

--- src/Orvian.Browser/SettingsWindow.xaml.cs
+++ src/Orvian.Browser/SettingsWindow.xaml.cs
@@ -25,10 +25,17 @@ public partial class SettingsWindow : Window
     }
 
     private void General_Click(object sender, RoutedEventArgs e) => Show("Allgemein", "Startseite, Suche und grundlegende Browseroptionen.", GeneralPanel);
-    private void Appearance_Click(object sender, RoutedEventArgs e) => Show("Darstellung", "Helles Design, Akzent und flüssige Orvian-Animationen.", AppearancePanel);
+    private void Appearance_Click(object sender, RoutedEventArgs e) => Show("Darstellung", "Design, Animationen und Orvian-Erscheinungsbild.", AppearancePanel);
     private void PrivacySettings_Click(object sender, RoutedEventArgs e) => Show("Datenschutz & Blocker", "Kontrolliere Werbung, Tracker und sichere Navigation.", PrivacyPanel);
     private void Security_Click(object sender, RoutedEventArgs e) => Show("Sicherheit", "Passwörter und Schutzfunktionen von Orvian.", SecurityPanel);
     private void Performance_Click(object sender, RoutedEventArgs e) => Show("Leistung", "Optimiere Speicher, Tabs und Hardwarebeschleunigung.", PerformancePanel);
     private void Help_Click(object sender, RoutedEventArgs e) => Show("Wie du den Browser nutzt", "Alle wichtigen Tastaturkürzel und Browseraktionen.", HelpPanel);
     private void About_Click(object sender, RoutedEventArgs e) => Show("Über Orvian", "Informationen zum Browserprojekt.", AboutPanel);
+
+    private void OpenVault_Click(object sender, RoutedEventArgs e)
+    {
+        new PasswordVaultWindow { Owner = this }.ShowDialog();
+    }
+
+    private void Close_Click(object sender, RoutedEventArgs e) => Close();
 }
